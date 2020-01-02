b2c4b3ffa1ef6c0f43d61ac1b44f91f1238e0711
Shota Shimazu <hornet.live.mf@gmail.com>
2020-01-02 18:11:56 +0900

    revise

--- strings/strings.go
+++ strings/strings.go
@@ -1,9 +1,6 @@
 package strings
 
 func Reverse(text string) string {
-	return "R"
-}
 
-func Inverse(text string) string {
-	return "B"
+	return "R"
 }
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -5,7 +5,3 @@ import "testing"
 func TestReverse(t *testing.T) {
 
 }
-
-func TestInverse(t *testing.T) {
-
-}
